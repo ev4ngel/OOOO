4f00e74b90ccd542db017b9abcbe057418b7d27f
westbest@gmail.com <westbest@gmail.com>
2013-11-08 00:18:21 +0800

    more over

--- ndone.py
+++ ndone.py
@@ -9,33 +9,44 @@ def done(url):
     bx=bs(html,parse_only=ss("div",id="content"))
     rlt=[]
     nexxt=bx.find(["a","img"])
+    cc=0
     while nexxt:
+        cc+=1    
         if nexxt.name=="a":
             try:
                 nexxt.img.get("src")
             except:
-                #print nexxt
                 if len(rlt)!=0:
-                    if not rlt[-1].get("tor",None):
-                        rlt[-1]["tor"]=nexxt.get("href")
+                    href=nexxt.get("href").strip()
+                    if not rlt[-1].get("tor",None) and re.search(r'[A-Z0-9]{6,10}\.html$',href) :
+                        rlt[-1]["tor"]=href
         elif nexxt.name=="img":
-            src=nexxt.get("src")
-            if re.search(r'jpg$',src,re.I):
+            src=nexxt.get("src").strip()
+            if re.search(r'jpg$',src,re.I):                
                 if len(rlt)==0 or  rlt[-1].get("tor",None):
                     rlt.append({"img":[src]})
                 else:
                     rlt[-1]["img"].append(src)
-        #print nexxt.name
         nexxt=nexxt.find_next(["a","img"])
     return rlt
-x=done("http://99.99btgongchang.info/p2p/01/12-12-31-10-58-28.html")
-def download_ex(ax,tgt_path):
+x=done("http://99.99btgongchang.info/p2p/01/13-01-01-23-16-48.html")
+def download_ex(ax,tgt_path,seperate_dir=False):
     for axx in ax:       
         dk=axx["tor"].split("/")[-1]
         print dk+"ing........."
-        tar=os.path.join(tgt_path,dk)
-        os.mkdir(tar)
-        write_torrent(axx['tor'],tar)
+        todir=tgt_path
+        if seperate_dir:
+            todir=os.path.join(tgt_path,dk)
+            os.mkdir(todir)
+        write_torrent(axx['tor'],todir)
         for img in axx['img']:
-            urllib.urlretrieve(img,os.path.join(tar,dk+"_"+img.split("/")[-1]))
+            t=os.path.join(todir,dk+"_"+img.split("/")[-1])
+            if not os.path.exists(t):
+                urllib.urlretrieve(img,t)
+def print_ex(ax):
+    for axx in ax:
+        print axx.get("tor")
+        print axx["img"]
+        print "*"*50
+#print_ex(x)
 download_ex(x,"e:/")
