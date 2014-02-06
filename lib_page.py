d9a7b7e444ae1e79bc28b4fba577ad2eaad1817e
westbest@gmail.com <westbest@gmail.com>
2014-02-06 23:37:00 +0800

    modify some detail alth on function download

--- lib_page.py
+++ lib_page.py
@@ -1,3 +1,4 @@
+# -*- coding: utf8 -*-
 from bs4 import BeautifulSoup as bs,SoupStrainer as ss
 import urllib2,urllib,re,os,urlparse
 from lib_page import *
@@ -41,7 +42,7 @@ def fromItemPage(url):
                         href=nexxt.get("href").strip()
                         if not rlt[-1].get("tor",None) and re.search(r'[A-Z0-9]{6,10}\.html$',href) :
                             rlt[-1]["tor"]=href
-                            rlt[-1]["purl"]=url
+                            #rlt[-1]["purl"]=url
             elif nexxt.name=="img":
                 src=nexxt.get("src").strip()
                 if re.search(r'jpg$',src,re.I):                
@@ -52,57 +53,81 @@ def fromItemPage(url):
             nexxt=nexxt.find_next(["a","img"])
     except:
         print "Failed to Get "+url
-    return rlt
+    return {url:rlt}
 
 def _None(url):
     pass
-def download_ax(ax,tgt_path,seperate_dir=False,
+def download_ax(ax,tgt_path,db_instance,seperate_dir=False,
                 ondisableimg=_None,
                 ondisabletor=_None,
                 onsuccessimg=_None,
                 onsuccesstor=_None,
                 onitem=_None):
     """
-    input: ax return by fromItemPage
+    input:{"page_url":[{tor:xxx,img:[xxx,xxx]},{tor:xxx,img:[xxx,xxx]},{url:xxx,tor:xxx,img:[xxx,xxx]}],"page_url":[]}
+        ax return by fromItemPage
             tgt_path the dir to put
             seperate_dir true if you want to mkdir for every item(one torrent and more pic)
     return:None for final use
     """
+    
     if not os.path.exists(tgt_path):
-        os.mkdir(tgt_path)
-    db=axDB(tgt_path)
-    for axx in ax:
-        tid=0
+        os.makedirs(tgt_path)
+    if db_instance.isUsed():
+        db_instance.connect()
+        for dpg in db_instance.getPages():
+            if int(dpg[2])==0:
+                del ax[dpg[1]]
+            else:
+                urls=[x[0] for x in db_instance.getTorrentsUrlByPageId(dpg[0])]       
+                for item in ax[dpg]:
+                    if item.get("tor",None) or item['tor'] in urls:
+                        del item
+        
+    for axk,axv in ax:#这里就可以放心大胆的存放所有剩余链接地址了
+        pid=db_instance.addPage(axk,"",0)
         todir=tgt_path
-        try:
-            dk=axx["tor"].split("/")[-1]
-            pid=db.getPageIdByUrl(axx['purl'])
-            if pid==-1:
-                pid=db.addPage(axx['purl'],"",1)            
+        for item in axv:
+            tor_rlt=0
+            dk=item["tor"].split("/")[-1]
             if seperate_dir:
                 todir=os.path.join(tgt_path,dk)
                 os.mkdir(todir)
-            print "[Tor]"+axx['tor']+"_ing..."
-            if db.getTorrentIdByName(dk)>0:
-                print "Existing,Cancel..."
-                continue
-            url,rlt=torrent_download(axx['tor'],todir)
-            print "OK:"+str(rlt)
-            tid=db.addTorrent(url,url.split("/")[-1],todir,int(rlt),pid)
-        except KeyError as ke:
-            print "No Torrent Found"
-        #onitem({'path':dk,"url":axx['tor'],"purl":axx['purl'],"img":axx['img']})
-        #if not rlt: 
-        #    ondisabletor({"url":url,"purl":axx['purl']})
-        try:
-            for img in axx['img']:
-                print "[Img]"+img.split("/")[-1]+"_ing..."
-                imgrlt=img_download(img,todir,dk+"_")
-                print "OK:"+str(imgrlt[0])
-                db.addImg(imgrlt[1],imgrlt[0],todir,int(imgrlt[2]),tid)
-                #if not imgrlt[2]:
-                #    ondisableimg([])
-                #else:
-                #    onsuccessimg([])
-        except KeyError as ke:
-            print "No Img Found"
+            url,rlt=torrent_download(item['tor'],todir)
+            if rlt:
+                tor_rlt=1
+            tid=db_instance.addTorrent(url,url.split("/")[-1],todir,int(tor_rlt),pid)
+            try:
+                for img in item['img']:
+                    imgrlt=img_download(img,todir,dk+"_")
+                    db.addImg(imgrlt[1],imgrlt[0],todir,int(imgrlt[2]),tid)
+            except KeyError as ke:
+                print "No Img Found"
+        db_instance.togglePageState(pid)
+            
+##        todir=tgt_path
+##        try:
+##            dk=axx["tor"].split("/")[-1]
+##            pid=db.getPageIdByUrl(axx['purl'])
+##            if pid==-1:
+##                pid=db.addPage(axx['purl'],"",1)            
+##            if seperate_dir:
+##                todir=os.path.join(tgt_path,dk)
+##                os.mkdir(todir)
+##            print "[Tor]"+axx['tor']+"_ing..."
+##            if db.getTorrentIdByName(dk)>0:
+##                print "Existing,Cancel..."
+##                continue
+##            url,rlt=torrent_download(axx['tor'],todir)
+##            print "OK:"+str(rlt)
+##            tid=db.addTorrent(url,url.split("/")[-1],todir,int(rlt),pid)
+##        except KeyError as ke:
+##            print "No Torrent Found"
+##        try:
+##            for img in axx['img']:
+##                print "[Img]"+img.split("/")[-1]+"_ing..."
+##                imgrlt=img_download(img,todir,dk+"_")
+##                print "OK:"+str(imgrlt[0])
+##                db.addImg(imgrlt[1],imgrlt[0],todir,int(imgrlt[2]),tid)
+##        except KeyError as ke:
+##            print "No Img Found"
